b8a9485c3f5ee1220acb306fc045aed3113eed59
xazuaje- <xazuaje-@student.42madrid.com>
2023-12-03 15:58:51 +0100

    bresenham

--- src/minilib/draw_line.c
+++ src/minilib/draw_line.c
@@ -6,38 +6,61 @@
 /*   By: xazuaje- <xazuaje-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 16:50:46 by xazuaje-          #+#    #+#             */
-/*   Updated: 2023/11/29 13:06:45 by xazuaje-         ###   ########.fr       */
+/*   Updated: 2023/11/29 13:06:45xazuaje-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilib.h"
+#include "unistd.h"
+
+typedef struct s_errors
+{
+	int		err;
+	int		e2;
+}			t_error;
+
+static void	adjust_err(int *diff, int *coords, int *signs,
+				t_error *error);
 
 void	draw_line(t_node *node1, t_node *node2, t_img *img)
 {
-	int	x;
-	int	y;
-	int	p;
-	int	i;
+	int		coords[2];
+	int		diff[2];
+	int		signs[2];
+	t_error	error;
 
-	i = 0;
-	x = node1->coords->x - node2->coords->x;
-	y = node1->coords->y - node2->coords->y;
-	if (y != 0)
-		p = x / y;
+	coords[0] = node1->coords->x;
+	coords[1] = node1->coords->y;
+	diff[0] = abs(node2->coords->x - coords[0]);
+	diff[1] = abs(node2->coords->y - coords[1]);
+	if (coords[0] < node2->coords->x)
+		signs[0] = 1;
 	else
-		p = 0;
-	x = node1->coords->x;
-	y = node2->coords->y;
-	while (x != (node2->coords->x + 1) && y != (node2->coords->y + p))
+		signs[0] = -1;
+	if (coords[1] < node2->coords->y)
+		signs[1] = 1;
+	else
+		signs[1] = -1;
+	error.err = diff[0] - diff[1];
+	while (coords[0] != node2->coords->x && coords[1] != node2->coords->y)
 	{
-		put_pixel(img, x, y, 0xFFFFFFFF);
-		if (i > p)
-		{
-			y += p;
-			x++;
-			i = 0;
-			continue ;
-		}
-		i++;
+		put_pixel(img, coords[0], coords[1], 0xFFAABB);
+		error.e2 = 2 * error.err;
+		adjust_err(diff, coords, signs, &error);
+	}
+}
+
+static void	adjust_err(int *diff, int *coords, int *signs,
+		t_error *error)
+{
+	if (error->e2 > -diff[1])
+	{
+		error->err -= diff[1];
+		coords[0] += signs[0];
+	}
+	if (error->e2 < diff[0])
+	{
+		error->err += diff[0];
+		coords[1] += signs[1];
 	}
 }
